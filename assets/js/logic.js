89572ae6f6044fc140ca8557f6e7cd4b3ec1a646
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2022-12-05 09:38:59 +0000

    init

---
+++ assets/js/logic.js
@@ -0,0 +1,14 @@
+var timerEl = document.getElementById('time')
+var btn = document.getElementById('start')
+
+
+function countdown() {
+    var timeLeft = 60
+    var timeInterval 
+};
+
+
+btn.addEventListener('click', function () {
+    console.log('hey, it works');
+
+});
