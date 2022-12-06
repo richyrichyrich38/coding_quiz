110eb5fada75be449bd79571d4129f08677d10e0
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2022-12-06 13:53:41 +0000

    some js updates inc time deduction and next question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,20 +2,21 @@
 var timerEl = document.getElementById('time')
 var btn = document.getElementById('start')
 var startScreen = document.getElementById('start-screen')
-var currentQuestionIndex = 0;
+var currentQuestionIndex = 0
+var score = 0
 var questionWrap = document.getElementById('questions')
 var questionTitle = document.getElementById('question-title')
 var choicesOutput = document.getElementById('choices')
+var counter;
 
 // adding countdown
 function startCountdown(seconds) {
-    var counter = seconds;
+    counter = seconds;
     var interval = setInterval(() => {
     // console.log(counter);
     counter--;
     if(counter < 1) {
         clearInterval(interval);
-        // showScores();
     }
     timerEl.innerHTML = `${counter}`;
     }, 1000);
@@ -28,14 +29,13 @@ btn.addEventListener('click', function () {
     startCountdown(75)
 });
 
-
+// function to cycle through questions
 function startQuiz() {
     var currentQuestion = questions[currentQuestionIndex];
     var choices = currentQuestion.choices;
-
     questionTitle.innerText = currentQuestion.title;
-
     choicesOutput.innerHTML = '';
+
 
     for(var i = 0; i < choices.length; i++) {
         var choice = choices[i];
@@ -44,15 +44,28 @@ function startQuiz() {
         choicesOutput.insertAdjacentHTML('beforeend', `
         <button data-correct=${isCorrect}>${choice}</button>
         `)
+
     }
 
 };
 
 function checkAnswer(event) {
 
-    console.log(event.target);
-};
+    console.log(event.target.dataset.correct);
+    var userChoice = event.target.dataset.correct;
 
+    if(userChoice == 'true') {
+        console.log(`Correct`);
+        currentQuestionIndex++
+        insertAdjacentHTML('')
+        startQuiz();
+    } else {
+        console.log(`Wrong`);
+        currentQuestionIndex++
+        startQuiz();
+        counter-= 15;
+    }
+};
 
 choicesOutput.addEventListener('click', checkAnswer)
 
